34410a0fbdb1a7100a76b3c50e4428da5273afd4
Jasper Kamerling <jasper.kamerling@alliander.com>
2025-03-11 10:18:03 +0100

    Fix chart relase script
    Signed-off-by: Jasper Kamerling <jasper.kamerling@alliander.com>

--- release.sh
+++ release.sh
@@ -2,5 +2,12 @@
 
 TOKEN=$1
 
-cr upload -o alliander-opensource -r pokertool-chart --push --generate-release-notes --token "$TOKEN"
+if [[ -z ${TOKEN} ]]; then
+  echo "Please provide a github token"
+  exit 1
+fi
+
+rm -r .cr-release-packages/
+cr package charts/pokertool/
+cr upload -o alliander-opensource -r pokertool-chart --push --generate-release-notes --token --commit "$(git rev-parse HEAD)" "$TOKEN"
 cr index  -o alliander-opensource -r pokertool-chart --push --index-path index.yaml --token "$TOKEN"
